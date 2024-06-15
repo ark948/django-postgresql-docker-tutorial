44ed8264c8b5c4cc044ad26b8bc74b5daa50f95f
ark948 <armankarimi948@gmail.com>
2024-06-15 18:16:59 +0330

    ch6, added login, logout, signup + tests

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,23 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    # local apps
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
+
+
+# accounts/login/ [name="login"]
+# accounts/logout/ [name="logout"]
+# accounts/password_change/ [name="password_change"]
+# accounts/password_change/done/ [name="password_change_done"]
+# accounts/password_reset/ [name="password_reset"]
+# accounts/password_reset/done/ [name="password_reset_done"]
+# accounts/reset/<uidb64>/<token>/ [name="password_reset_confirm"]
+# accounts/reset/done/ [name="password_reset_complete"]
